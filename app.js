44540127218f8320c5a7a4c929b1262c498d143c
notegac <n.ortegac@uniandes.edu.co>
2021-03-18 22:16:08 -0500

    Lint corrections 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const app = express();
 
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(PORT, () => {
-  console.log("Server started on port 5000");
+  console.log('Server started on port 5000');
 });
